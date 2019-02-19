f30ea616da561f9ce29dc8472303b66e7b8f2dca
Leonie Lefebvre <leonie.lefebvre@outlook.com>
2019-02-19 18:50:01 +0100

    petites modifs

--- app/controllers/users_controllers.rb
+++ app/controllers/users_controllers.rb
@@ -3,19 +3,23 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
 
-  # def create
-  #   @user = User.new(user_params)
-  #   @user.save
-  # end
+  def edit
+  end
+
+  def destroy
+  end
+
+  private
 
   def user_params
     params.require(:user).permit(:name, :email, :description)
   end
 end
-
-# :reset_password_token
-# :reset_password_token
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'dogs#index'
   resources :dogs, only: [:index, :create, :new, :show]
+  resources :users, only: [:index, :edit, :show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
